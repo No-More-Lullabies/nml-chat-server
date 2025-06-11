ed24393afd9461a1225ef42be2cd3330dd927972
No-More-Lullabies <therealbrandonwright@gmail.com>
2025-06-11 19:30:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,23 @@ const PORT = process.env.PORT || 3000;
 const server = new WebSocket.Server({ port: PORT });
 
 server.on('connection', socket => {
-  socket.on('message', message => {
+  socket.on('message', async (message) => {
+    let text;
+
+    if (typeof message === 'string') {
+      text = message;
+    } else if (message instanceof Buffer) {
+      text = message.toString('utf-8');
+    } else if (message.arrayBuffer) {
+      const buffer = await message.arrayBuffer();
+      text = Buffer.from(buffer).toString('utf-8');
+    } else {
+      text = '[Unrecognized message format]';
+    }
+
     server.clients.forEach(client => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        client.send(text);
       }
     });
   });
